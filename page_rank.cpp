fbfc47c9834e17902e84cd5f327dddff9ab450ff
raccoon <raccoon1011227@gmail.com>
2020-11-16 16:53:40 +0800

    1116 upload

---
+++ page_rank.cpp
@@ -0,0 +1,83 @@
+#include "page_rank.h"
+
+#include <stdlib.h>
+#include <cmath>
+#include <omp.h>
+#include <utility>
+
+#include "../common/CycleTimer.h"
+#include "../common/graph.h"
+
+// pageRank --
+//
+// g:           graph to process (see common/graph.h)
+// solution:    array of per-vertex vertex scores (length of array is num_nodes(g))
+// damping:     page-rank algorithm's damping parameter
+// convergence: page-rank algorithm's convergence threshold
+//
+void pageRank(Graph g, double *solution, double damping, double convergence)
+{
+
+  // initialize vertex weights to uniform probability. Double
+  // precision scores are used to avoid underflow for large graphs
+
+  int numNodes = num_nodes(g);
+  int converged = 0;
+  double *score_old, *score_new;
+  double equal_prob = 1.0 / numNodes;
+  for (int i = 0; i < numNodes; ++i)
+  {
+    solution[i] = equal_prob;
+  }
+  for(int i = 0; i < numNodes; i++)
+  {
+    score_old[i] = 1/numNodes;
+    score_new[i] = 0;
+  }
+  while(!converged){
+    for(int i = 0; i < numNodes; i++){
+      const Vertex* start = incoming_begin(g, i);
+      const Vertex* end = incoming_end(g, i);
+      for(const Vertex* j = start; j != end; j++){
+        score_new[i] += score_old[*j] / outgoing_size(g, *j);
+      }
+      score_new[i] = damping * score_new[i] + (1.0 - damping) / numNodes;
+      for(int k = 0; k < numNodes; k++){
+      	if(outgoing_size(g, k) == 0)
+      	  score_new[i] += damping * score_old[k] / numNodes;
+      }
+    }
+    int global_diff = 0;
+    for(int i = 0; i < numNodes; i++)
+      global_diff += abs(score_new[i] - score_old[i]);
+    converged = (global_diff < convergence);
+  }
+  /*
+     For PP students: Implement the page rank algorithm here.  You
+     are expected to parallelize the algorithm using openMP.  Your
+     solution may need to allocate (and free) temporary arrays.
+
+     Basic page rank pseudocode is provided below to get you started:
+
+     // initialization: see example code above
+     score_old[vi] = 1/numNodes;
+
+     while (!converged) {
+
+       // compute score_new[vi] for all nodes vi:
+       score_new[vi] = sum over all nodes vj reachable from incoming edges
+                          { score_old[vj] / number of edges leaving vj  }
+       score_new[vi] = (damping * score_new[vi]) + (1.0-damping) / numNodes;
+
+       score_new[vi] += sum over all nodes v in graph with no outgoing edges
+                          { damping * score_old[v] / numNodes }
+
+       // compute how much per-node scores have changed
+       // quit once algorithm has converged
+
+       global_diff = sum over all nodes vi { abs(score_new[vi] - score_old[vi]) };
+       converged = (global_diff < convergence)
+     }
+
+   */
+}
